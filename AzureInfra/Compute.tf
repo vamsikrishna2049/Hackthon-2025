8c8b866e8bca4f0b655da0102d13b16929727be7
Vamsi krishna <118727057+vamsikrishna2049@users.noreply.github.com>
2025-07-12 20:19:55 +0530

    Create Compute.tf

---
+++ AzureInfra/Compute.tf
@@ -0,0 +1,54 @@
+# ----------------------
+# compute.tf
+# ----------------------
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "atm-private-vm"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  size                  = "Standard_B1ms"
+  admin_username        = "azureuser"
+  network_interface_ids = [azurerm_network_interface.vm_nic.id]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_public_ip" "bastion_ip" {
+  name                = "atm-bastion-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = var.tags
+}
+
+resource "azurerm_bastion_host" "bastion" {
+  name                = "atm-bastion"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Basic"
+
+  ip_configuration {
+    name                 = "bastion-ip-config"
+    subnet_id            = azurerm_subnet.bastion.id
+    public_ip_address_id = azurerm_public_ip.bastion_ip.id
+  }
+
+  tags = var.tags
+}
