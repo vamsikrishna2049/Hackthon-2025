f110ce489704fc7da8da91d710a21977c15d8881
Vamsi krishna <118727057+vamsikrishna2049@users.noreply.github.com>
2025-07-14 06:40:48 +0000

    ProviderAccess - Service Priniciapl

--- AzureInfra/provider.tf
+++ AzureInfra/provider.tf
@@ -9,12 +9,19 @@ terraform {
 
 provider "azurerm" {
   features {}
+  # Inline Authentication Not a Better Practise - we're login using Service Principal
+
   #https://portal.azure.com/#view/Microsoft_Azure_Billing/SubscriptionsBladeV2
-  subscription_id = "61b3ba1a-d345-44f6-a043-200a4e5ae92b"
-
-  tenant_id = "b1beb41-ebc0-4f8d-b748-bf4917a6b194"
-
-  client_id = "0a4663e-10ff-4298-8948-8c0ef5c4b861"
-
-  client_secret = "p-o8Q~4TxH5UFdEk-x82vGddWeWsG6IkSF9nBdcB"
+  subscription_id = "####################"
+  tenant_id       = "####################"
+  client_id       = "####################"
+  client_secret   = "####################"
 }
+
+
+# Authentication test - Service Principal Working/not
+output "subscription_id" {
+  value = data.azurerm_client_config.current.subscription_id
+}
+
+data "azurerm_client_config" "current" {}
